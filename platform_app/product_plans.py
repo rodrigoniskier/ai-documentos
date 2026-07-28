53273cb0b0e0d75a7648525c834d041503eb691c
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-07-28 10:08:18 -0300

    Modera aumento dos limites dos planos pagos

--- platform_app/product_plans.py
+++ platform_app/product_plans.py
@@ -4,17 +4,17 @@ from .models import Plan
 PAID_PLAN_LIMITS = {
     "PRO": {
         "monthly_credits": 60,
-        "institution_limit": 4,
-        "discipline_limit": 20,
-        "source_limit": 60,
-        "daily_limit": 20,
+        "institution_limit": 3,
+        "discipline_limit": 15,
+        "source_limit": 40,
+        "daily_limit": 16,
     },
     "PREMIUM": {
-        "monthly_credits": 180,
-        "institution_limit": 12,
-        "discipline_limit": 60,
-        "source_limit": 200,
-        "daily_limit": 60,
+        "monthly_credits": 160,
+        "institution_limit": 7,
+        "discipline_limit": 40,
+        "source_limit": 140,
+        "daily_limit": 40,
     },
 }
 
